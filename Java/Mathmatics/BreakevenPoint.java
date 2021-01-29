d51e5c749f6faaff9b67860af2e4b760fff62881
Su Ha Lee <40999647+LeeSuHa98@users.noreply.github.com>
2021-01-29 18:43:57 +0900

    solve mathmatics problem

---
+++ Java/Mathmatics/BreakevenPoint.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BreakevenPoint {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A, B, C = 0;
+        int count = 0;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+        C = sc.nextInt();
+
+        sc.close();
+
+        if(B >= C){
+            System.out.println("-1");
+        }
+        else{
+            System.out.println(A / ( C - B) + 1);
+        }
+    }
+}

34df2f03c12c46c3d24141aad78ba221e962316e
Su Ha Lee <40999647+LeeSuHa98@users.noreply.github.com>
2021-02-01 15:36:03 +0900

    solve baekjoon N.10757 use BigInteger data type

---
+++ Java/Mathmatics/Sum.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Sum {
+    public static void main(String[] args) throws IllegalStateException{
+        Scanner sc = new Scanner(System.in);
+        
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+
+        System.out.println(a.add(b));
+    }
+}

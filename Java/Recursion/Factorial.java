b849f11f7d1e2e9e952ebca41bc3ee1225a222e3
Su Ha Lee <40999647+LeeSuHa98@users.noreply.github.com>
2021-02-09 15:20:32 +0900

    solve factorial problem use recursion

---
+++ Java/Recursion/Factorial.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        sc.close();
+        
+        System.out.println(factorial(N));
+    }
+
+    public static int factorial(int n){
+        if(n == 0){
+            return 1;
+        }
+        return n * factorial(n-1);
+    }
+}

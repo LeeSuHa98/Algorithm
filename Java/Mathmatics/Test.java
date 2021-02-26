b06b766d915cdad1407df7e8500d355be629e6d5
Su Ha Lee <40999647+LeeSuHa98@users.noreply.github.com>
2021-02-26 15:30:28 +0900

    solve demo Test

--- Java/Mathmatics/Test.java
+++ Java/Mathmatics/Test.java
@@ -1,10 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class Test {
-    Long sum(int[] a){
-        long sum = 0;
-        
-        for(int i = 0; i < a.length; i++){
-            sum += a[i];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String x = br.readLine();
+
+        int bumbCount = 0;
+        int defenceCount = 0;
+
+        for(int i = 0; i < x.length(); i++){
+            if(x.charAt(i) == '('){
+                bumbCount++;
+            }
+            else if(x.charAt(i) == ')'){
+                defenceCount++;
+            }
         }
-        return sum;
+
+        if(defenceCount == bumbCount){
+            System.out.println("YES");
+        }
+        else if(defenceCount > bumbCount || defenceCount < bumbCount){
+            System.out.println("NO");
+        }
     }
 }

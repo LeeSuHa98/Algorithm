a2d22f375d074e0eaa3a624bb8846d4a4f9111ac
Su Ha Lee <40999647+LeeSuHa98@users.noreply.github.com>
2021-03-04 21:48:56 +0900

    solve hanoi tower problem

--- Java/Mathmatics/Test.java
+++ Java/Mathmatics/Test.java
@@ -1,31 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 
 public class Test {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // public static boolean visit;
+    // public static char[][] check;
 
-        String x = br.readLine();
+    // public static void main(String[] args) throws IOException{
+    //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //     StringBuilder sb = new StringBuilder();
+    //     StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
-        int bumbCount = 0;
-        int defenceCount = 0;
+    //     String K = st.nextToken();
+    //     int N = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < x.length(); i++){
-            if(x.charAt(i) == '('){
-                bumbCount++;
+    //     check = new char[N][N];
+
+    //     for(int i = 1; i < N+1; i++){
+    //         st = new StringTokenizer(br.readLine());
+
+    //         String a = st.nextToken();
+    //         String b = st.nextToken();
+
+    //         check[i][b] = 
+    //     }
+    // }
+    public static void main(String[] args) throws IOException {
+ 
+		Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        
+
+        int[] arr = new int[n];
+        for (int i = 0; i <n ; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = 0;
+        Arrays.sort(arr);
+
+        int left =1;
+        int right =arr[n-1];
+        long height =0;
+        int mid =0;
+        int ans=0;
+
+        int result = 0;
+        while(left <= right){
+
+            height =0;
+            mid =(left+right)/2;
+
+            for (int i = 0; i <n ; i++) {
+                if(arr[i]>=mid) {
+                    result += arr[i];
+                }
             }
-            else if(x.charAt(i) == ')'){
-                defenceCount++;
+            
+            if(right < left){
+
+                left = mid + 1;
+            }else {
+                right = mid-1;
+
             }
         }
-
-        if(defenceCount == bumbCount){
-            System.out.println("YES");
-        }
-        else if(defenceCount > bumbCount || defenceCount < bumbCount){
-            System.out.println("NO");
-        }
+        System.out.println(result);
     }
 }

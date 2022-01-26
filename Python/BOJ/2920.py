12d64c8db437d6fd305214d7b548d29d795d581c
LeeSuHa98 <lapi12@naver.com>
2022-01-26 18:30:46 +0900

    solve 2920

---
+++ Python/BOJ/2920.py
@@ -0,0 +1,30 @@
+# solve 1
+# sort code and effective
+
+N = input().split(" ")
+result = []
+for i in range(1, 8):
+    check = int(N[i]) - int((N[i-1]))
+    if abs(check) == 1:
+        result.append(check)
+    else:
+        result.append(0)
+
+if result.count(1) == 7:
+    print('ascending')
+elif result.count(-1) == 7:
+    print('descending')
+else:
+    print('mixed')
+    
+# solve 2
+# first code uneffective
+
+N = list(map(int, input().split()))
+
+if N == sorted(N):
+    print('ascending')
+elif N == sorted(N, reverse=True):
+    print('descending')
+else:
+    print('mixed')

1384bb10af146d1b5b7230148d4b72c8238e6460
LeeSuHa98 <lapi12@naver.com>
2022-02-14 22:08:01 +0900

    solve dfs algorithm

---
+++ Python/BOJ/2606.py
@@ -0,0 +1,29 @@
+#dfs
+
+def dfs(gragh, v, visited):
+    visited[v] = True
+    global count
+    for i in gragh[v]:
+        if not visited[i]:
+            dfs(gragh, i, visited)
+            count += 1
+            
+N = int(input())
+network = int(input())
+
+gragh = [[]*N for i in range(N+1)]
+
+#[0][i] 는 리스트 번호
+#[j][i]는 j 리스트 번호에 들어갈 배열
+for i in range(network):
+    a, b = map(int, input().split())
+    gragh[a].append(b)
+    print(a, "====", gragh)
+    gragh[b].append(a)
+    print(b, "====", gragh)
+    
+count = 0
+visited = [False]*(N+1)
+
+dfs(gragh, 1, visited)
+print(count)

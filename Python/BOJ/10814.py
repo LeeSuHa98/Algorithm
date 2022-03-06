2acf246dbd02e5d0f538c1a15bd1afc0de55e29d
LeeSuHa98 <lapi12@naver.com>
2022-03-06 12:22:48 +0900

    solve algorithm
    백준 10814 나이순정렬
    + 람다로 정렬

---
+++ Python/BOJ/10814.py
@@ -0,0 +1,14 @@
+import sys
+sys.setrecursionlimit(10000)
+
+N = int(sys.stdin.readline())
+
+array = []
+for i in range(N):
+    age, name = map(str, sys.stdin.readline().split())
+    array.append([int(age), name])
+
+array = sorted(array, key=lambda x: x[0])
+
+for i in range(N):
+    print(array[i][0], array[i][1])

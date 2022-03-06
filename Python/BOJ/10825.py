f69d526724b40a86d23ab9ef18d22736237050f8
LeeSuHa98 <lapi12@naver.com>
2022-03-06 11:59:01 +0900

    solve algorithm
    백준 10825 국영수
    + sorted key에 lambda 함수 사용

---
+++ Python/BOJ/10825.py
@@ -0,0 +1,14 @@
+import sys
+sys.setrecursionlimit(10000)
+
+N = int(sys.stdin.readline())
+
+array = []
+for i in range(N):
+    name, koean, english, math = map(str, sys.stdin.readline().split())
+    array.append([name, int(koean), int(english), int(math)])
+
+array = sorted(array, key=lambda x: (-x[1], x[2], -x[3], x[0]))
+
+for i in range(N):
+    print(array[i][0])

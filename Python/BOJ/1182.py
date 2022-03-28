7b8a64d518de2871aabae5af14c795f03008d3cb
LeeSuHa98 <lapi12@naver.com>
2022-03-28 11:06:21 +0900

    백준 1182 풀이 추가
    + 브루트포스 이진법 풀이
    + 비트연산자, 시프트연산자 이용

--- Python/BOJ/1182.py
+++ Python/BOJ/1182.py
@@ -1,5 +1,7 @@
 import sys
 
+# solve 1 재귀함수
+
 def dfs(index, sum):
     global count
     
@@ -19,4 +21,17 @@ array = list(map(int, sys.stdin.readline().split()))
 
 count = 0
 dfs(0, 0)
+print(count)
+
+# solve 2 브루트포스 이진법
+
+N, K = map(int, sys.stdin.readline().split())
+array = list(map(int, sys.stdin.readline().split()))
+count = 0
+
+for i in range(1, 1 << N):
+    t = 0
+    for j in range(N):
+        if i & 1 << j: t += array[j]
+    if t == K: count += 1
 print(count)

17f6f0734c5d2866acd160b78bf384e90a308e7e
LeeSuHa98 <lapi12@naver.com>
2022-06-08 21:34:15 +0900

    solve

---
+++ JavaScript/수 정렬하기2.js
@@ -0,0 +1,5 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+
+const [N, ...numList] = input.map((e) => Number(e));
+const result = numList.sort((a, b) => a - b);
+console.log(result.join('\n'));
